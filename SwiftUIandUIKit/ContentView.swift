aa930531ef43060cf180e0aebe920f6b6c239304
KaterinaKugeneva <kugeneva1@gmail.com>
2022-02-01 12:27:58 +0300

    main functional added

--- SwiftUIandUIKit/ContentView.swift
+++ SwiftUIandUIKit/ContentView.swift
@@ -7,10 +7,47 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    @State private var targetValue: Int = Int.random(in: 0...100)
+    @State private var value: Double = Double.random(in: 0...100)
+    @State private var alertPresented = false
+    
     var body: some View {
-        Text("Hello, world!")
+        
+        VStack{
+            Spacer()
+            Text("Подвиньте слайдер как можно ближе к \(targetValue)")
+                .padding()
+            HStack{
+                Text("0")
+                SliderView(value: $value, targetValue: $targetValue)
+                    .opacity((value) / Double(targetValue))
+                Text("100")
+            }.padding()
+            Button("Показать результат") {
+                alertPresented.toggle()
+            }
+            .alert(isPresented:$alertPresented) {
+                Alert(
+                    title: Text("Ваш результат:"),
+                    message: Text("\(computeScore())")
+                )
+            }
             .padding()
+            
+            Button("Начать заново") {
+                targetValue = Int.random(in: 0...100)
+            }
+            Spacer()
+            
+        }
+    }
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - lround(value))
+        return 100 - difference
+        
     }
 }
 
---
+++ SwiftUIandUIKit/SliderView.swift
@@ -0,0 +1,57 @@
+//
+//  Slider.swift
+//  SwiftUIandUIKit
+//
+//  Created by Ekaterina Kugeneva on 01.02.2022.
+//
+
+import SwiftUI
+
+struct SliderView: UIViewRepresentable {
+
+    @Binding var value : Double
+    @Binding var targetValue : Int
+
+    class Coordinator: NSObject {
+        var sliderView: SliderView
+
+        init(_ sliderView: SliderView) {
+            self.sliderView = sliderView
+        }
+
+        @objc func valueChanged(_ slider: UISlider) {
+            sliderView.value = Double(slider.value)
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIView(context: Context) -> UISlider {
+        let slider = UISlider(frame: .zero)
+        slider.minimumValue = 0.00
+        slider.maximumValue = 100.00
+        slider.value = Float(value)
+        slider.thumbTintColor = .red
+        //slider.layer.opacity = Float(value) / Float(targetValue)
+
+        slider.addTarget(
+             context.coordinator,
+             action: #selector(Coordinator.valueChanged(_:)),
+             for: .valueChanged
+           )
+
+        return slider
+    }
+
+    func updateUIView(_ uiView: UISlider, context: Context) {
+
+    }
+}
+
+struct SliderView_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderView(value: .constant(100),targetValue: .constant(100))
+    }
+}
