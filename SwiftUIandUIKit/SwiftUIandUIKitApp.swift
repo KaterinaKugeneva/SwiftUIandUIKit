44eb10a2606ada0229dc93b2890f1d291fa6c358
KaterinaKugeneva <kugeneva1@gmail.com>
2022-02-01 09:37:43 +0300

    Initial Commit

---
+++ SwiftUIandUIKit/SwiftUIandUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIandUIKitApp.swift
+//  SwiftUIandUIKit
+//
+//  Created by Ekaterina Kugeneva on 01.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIandUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
